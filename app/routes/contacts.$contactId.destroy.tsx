f8900f081f477aed77c1631b2590d5272089df63
Teerawat P. <teewat@yahoo.com>
2024-03-03 14:20:11 +1100

    Learn: remix day1

---
+++ app/routes/contacts.$contactId.destroy.tsx
@@ -0,0 +1,10 @@
+import { ActionFunctionArgs, redirect } from '@remix-run/node';
+import { deleteContact } from '../data';
+import invariant from 'tiny-invariant';
+
+export const action = async ({ params }: ActionFunctionArgs) => {
+  invariant(params.contactId, 'Missing contactId param');
+
+  await deleteContact(params.contactId);
+  return redirect(`/`);
+};
